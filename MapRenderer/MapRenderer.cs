fc7424f2643bf0ce0075044dd8b0cd3de18eb898
Keith Simmons <keith@the-simmons.net>
2015-12-18 05:05:00 -0800

    Colors

--- MapRenderer/MapRenderer.cs
+++ MapRenderer/MapRenderer.cs
@@ -1,18 +1,22 @@
 ﻿using Microsoft.Xna.Framework;
 using MonoGame.Interop.Modules;
+using System;
 
 namespace MapRenderer
 {
     public class MapRenderer : BaseGameModule
     {
+        Random rand;
 
         public MapRenderer()
           : base("Content")
-        { }
+        {
+            rand = new Random();
+        }
 
         public override void Draw()
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(new Color(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255)));
         }
     }
 }
